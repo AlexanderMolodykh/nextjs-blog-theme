fdc60124044050384abe19ab44d1e733763ce317
AlexanderMolodykh <107105078+AlexanderMolodykh@users.noreply.github.com>
2022-06-09 10:49:37 +0200

    Update themes.js

--- themes.js
+++ themes.js
@@ -1,47 +1,27 @@
 exports.COLOR_THEMES = {
   default: {
     colors: {
-      primary: '#7D7AFF',
-      'gradient-1': '#7d7aff',
-      'gradient-2': '#2121e2',
-      'gradient-3': '#00fff0',
-      'gradient-4': '#8785FF',
+      primary: '#7D7AFF'
     },
   },
   bejamas: {
     colors: {
-      primary: '#FF8585',
-      'gradient-1': '#7d7aff',
-      'gradient-2': '#2121E2',
-      'gradient-3': '#FF76B8',
-      'gradient-4': '#001AFF',
+      primary: '#FF8585'
     },
   },
   netlify: {
     colors: {
-      primary: '#00A354',
-      'gradient-1': '#00F0FF',
-      'gradient-2': '#00F0FF',
-      'gradient-3': '#FAFF00',
-      'gradient-4': '#00F0FF',
+      primary: '#00A354'
     },
   },
   reddie: {
     colors: {
-      primary: '#FF4D4D',
-      'gradient-1': '#FFC700',
-      'gradient-2': '#FF85DD',
-      'gradient-3': '#FF85DD',
-      'gradient-4': '#FF8585',
+      primary: '#FF4D4D'
     },
   },
   greenie: {
     colors: {
-      primary: '#C78500',
-      'gradient-1': '#FFCC81',
-      'gradient-2': '#00F37F',
-      'gradient-3': '#00F37F',
-      'gradient-4': '#FFCC81',
+      primary: '#C78500'
     },
   },
 };
