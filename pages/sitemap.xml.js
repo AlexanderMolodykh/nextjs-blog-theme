ad679f1d7ba0f8f0262f2e02aba0f0622d7a34a3
Alex <alexander.molodih@gmail.com>
2022-06-15 07:45:38 +0200

    test

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -8,7 +8,7 @@ const Sitemap = () => {
 
 export const getServerSideProps = async ({ res }) => {
     const BASE_URL = 'https://tech.molodykh.online';
-
+try{
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       ${getPosts()
@@ -29,7 +29,10 @@ const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     res.setHeader('Content-Type', 'text/xml');
     res.write(sitemap);
     res.end();
-
+} catch (error) {
+	res.write(error.stack);
+    res.end();
+}
     return {
         props: {},
     };
