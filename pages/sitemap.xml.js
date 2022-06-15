d0e97f8ab3bc24e0a33ad8acd04548d4dfe72fa6
Alex <alexander.molodih@gmail.com>
2022-06-15 08:38:17 +0200

    hack

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,63 +1,67 @@
 import React from "react";
-import fs from "fs";
-import path from "path";
-import matter from "gray-matter";
 
 const Sitemap = () => {
     return null;
 };
 
-// POSTS_PATH is useful when you want to get the path to a specific file
-export const POSTS_PATH = path.join(process.cwd(), 'posts');
-
-// postFilePaths is the list of all htm files inside the POSTS_PATH directory
-export const postFilePaths = fs
-  .readdirSync(POSTS_PATH)
-  .filter((path) => /\.htm?$/.test(path));
-
-export const getPosts = () => {
-  let posts = postFilePaths.map((filePath) => {
-    const source = fs.readFileSync(path.join(POSTS_PATH, filePath));
-    const { content, data } = matter(source);
-
-    return {
-      content,
-      data,
-      filePath,
-    };
-  });
-
-  return posts;
-};
-
 export const getServerSideProps = async ({ res }) => {
-    const BASE_URL = 'https://tech.molodykh.online';
-try{
+
 const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${getPosts()
-        .map((post) => {
-          return `
-            <url>
-              <loc>${BASE_URL+"/posts/" + post.filePath.replace(/\.htm?$/, '') + "/"}</loc>
-              <lastmod>${new Date(post.data.date)}</lastmod>
-              <changefreq>yearly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `;
-        })
-        .join("")}
-    </urlset>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+<url>
+<loc>https://tech.molodykh.online/posts/animation-in-xaml/</loc>
+<lastmod>Sun Apr 08 2012 00:00:00 GMT+0200 (czas środkowoeuropejski letni)</lastmod>
+<changefreq>yearly</changefreq>
+<priority>1.0</priority>
+</url>
+<url>
+<loc>https://tech.molodykh.online/posts/inheritance-principles/</loc>
+<lastmod>Tue Jun 01 2010 00:00:00 GMT+0200 (czas środkowoeuropejski letni)</lastmod>
+<changefreq>yearly</changefreq>
+<priority>1.0</priority>
+</url>
+<url>
+<loc>https://tech.molodykh.online/posts/ipv4-operations/</loc>
+<lastmod>Mon Jul 05 2010 00:00:00 GMT+0200 (czas środkowoeuropejski letni)</lastmod>
+<changefreq>yearly</changefreq>
+<priority>1.0</priority>
+</url>
+<url>
+<loc>https://tech.molodykh.online/posts/jqplugin/</loc>
+<lastmod>Sat Aug 07 2010 00:00:00 GMT+0200 (czas środkowoeuropejski letni)</lastmod>
+<changefreq>yearly</changefreq>
+<priority>1.0</priority>
+</url>
+<url>
+<loc>https://tech.molodykh.online/posts/path-markup-test/</loc>
+<lastmod>Mon Mar 04 2013 00:00:00 GMT+0100 (czas środkowoeuropejski standardowy)</lastmod>
+<changefreq>yearly</changefreq>
+<priority>1.0</priority>
+</url>
+<url>
+<loc>https://tech.molodykh.online/posts/templated-control-xaml-2/</loc>
+<lastmod>Wed May 16 2012 00:00:00 GMT+0200 (czas środkowoeuropejski letni)</lastmod>
+<changefreq>yearly</changefreq>
+<priority>1.0</priority>
+</url>
+<url>
+<loc>https://tech.molodykh.online/posts/templated-control-xaml/</loc>
+<lastmod>Thu May 03 2012 00:00:00 GMT+0200 (czas środkowoeuropejski letni)</lastmod>
+<changefreq>yearly</changefreq>
+<priority>1.0</priority>
+</url>
+<url>
+<loc>https://tech.molodykh.online/posts/wcf-pony/</loc>
+<lastmod>Sat May 31 2014 00:00:00 GMT+0200 (czas środkowoeuropejski letni)</lastmod>
+<changefreq>yearly</changefreq>
+<priority>1.0</priority>
+</url>
+</urlset>
 `;
 
     res.setHeader('Content-Type', 'text/xml');
     res.write(sitemap);
     res.end();
-} catch (error) {
-	res.write(error);
-	res.write(error.stack);
-    res.end();
-}
     return {
         props: {},
     };
