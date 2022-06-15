97bfd07a3a52a56e3afb9d7c6d96953ee7e5e8c3
Alex <alexander.molodih@gmail.com>
2022-06-15 22:48:00 +0200

    revert

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { getGlobalData } from '../utils/global-data';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en" className="theme-compiled">
         <Head>
-	    <title>{globalData.name}</title>
-	</Head>
+        </Head>
         <body
           className={`antialiased text-lg bg-white dark:bg-gray-900 dark:text-white leading-base`}
         >
